a38b1eb7e3d850046b8202abb49f33e4e881854b
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-09-25 14:57:33 +0200

    dockerfiles init

---
+++ cli.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+git_upt() {
+    git submodule update --init --recursive
+}
+
+kube() {
+    kubectl apply -f kubernetes/
+}
+
+dc() {
+    docker-compose up -d
+}
+
+display_help() {
+    echo "   -h, --help          Display help"
+    echo "   git-upt             Update all git submodules"
+    echo "   kube                Deploy kubernetes resources"
+    echo "   dc                  Deploy docker-compose resources"
+    exit 1
+}
+
+# Main Script
+case "$1" in
+    git-upt)
+        git_upt
+        ;;
+    kube)
+        kube
+        ;;
+    dc)
+        dc
+        ;;
+    help | --help | -h)
+        display_help
+        ;;
+    *)
+        echo "Command not found"
+        ;;
+esac
